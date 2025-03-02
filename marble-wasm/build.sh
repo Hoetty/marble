2d808f56e212f8ea6613c733e4019ab0382d6853
Hoetty <kilian.behnke@outlook.com>
2025-03-02 12:58:06 +0100

    Correct Workflow v2

--- marble-wasm/build.sh
+++ marble-wasm/build.sh
@@ -1,6 +1,3 @@
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-cd "$SCRIPT_DIR" || exit
-
 rustup target add wasm32-unknown-unknown
 cargo install wasm-bindgen-cli
 cargo build --target wasm32-unknown-unknown --release
