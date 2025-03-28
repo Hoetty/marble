6a6ef903d7355c7f24f0981cae6c133fbee63cda
Hoetty <kilian.behnke@outlook.com>
2025-03-28 23:04:42 +0100

    Build

--- marble-wasm/build.sh
+++ marble-wasm/build.sh
@@ -1,4 +1,4 @@
 rustup target add wasm32-unknown-unknown
 cargo install wasm-bindgen-cli
 cargo build --target wasm32-unknown-unknown --release
-wasm-bindgen --out-dir ../marble-editor/wasm --target web ./target/wasm32-unknown-unknown/release/marble_wasm.wasm
+wasm-bindgen --out-dir ../marble-editor/wasm --target web target/wasm32-unknown-unknown/release/marble_wasm.wasm
