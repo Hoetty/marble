d8cbd29d3dcd3273fe60964bf027d7a8f8430a1c
Hoetty <kilian.behnke@outlook.com>
2025-03-29 22:54:50 +0100

    Reposition Doc Comment

--- src/number/mod.rs
+++ src/number/mod.rs
@@ -1,3 +1,22 @@
+//! A crate to handle numbers and their written forms
+//!  
+//! Provides utility to convert numbers to words
+//! ```rust
+//! use marble::number::serialize;
+//! assert_eq!(serialize::display_number(123), "OneHundredTwentyThree");
+//! ```
+//!
+//! And utility to parse such strings to numbers
+//!
+//! ```rust
+//! use marble::number::deserialize;
+//! assert_eq!(deserialize::parse_number("OneHundredTwentyThree"), Some(123));
+//! ```
+//!
+//! Please note, that serialization will always return a string directly,
+//! because all numbers can be converted to a word. However in contrast, desirialization
+//! returns an Option, as it may fail. *```"Banana"``` isn't a number after all*.
+
 pub mod deserialize;
 pub mod serialize;
 
